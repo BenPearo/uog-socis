d7086081687af0f45742cc1048dd6efebffd3b2e
Ben Pearo <benpearo@gmail.com>
2020-05-11 17:45:13 -0400

    added some styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Navbar, { NavItem } from './Navbar'
+
 export default class Header extends React.Component {
 	render () {
 		return (
-			<div>This is the header</div>
+			<Navbar>
+				<NavItem heading='Events' route=''></NavItem>
+				<NavItem heading='GCC' route=''></NavItem>
+				<NavItem heading='Roboticon' route=''></NavItem>
+				<NavItem heading='CUSEC' route=''></NavItem>
+				<NavItem heading='CS Games' route=''></NavItem>
+				<NavItem heading='About' route=''></NavItem>
+			</Navbar>
 		)
 	}
 }
